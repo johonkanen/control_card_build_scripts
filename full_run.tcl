1a16d3fb40e273829db18412a7f2ab56174ca378
Jari Honkanen <jaarhonka@yahoo.com>
2025-01-21 12:45:25 +0200

    remove application specific io from place_io.tcl

--- full_run.tcl
+++ full_run.tcl
@@ -13,33 +13,29 @@ run_tcl_scripts $tcl_path/ip_generation
 
 source $source_folder/list_of_sources.tcl
 
-set_property top top [current_fileset]
+source $tcl_path/place_io.tcl
 
-#synth_design
-#
-#source $tcl_path/place_io.tcl
-#
-#write_checkpoint -force $outputDir/post_synth.dcp
-#report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
-#report_utilization -file $outputDir/post_synth_util.rpt
-#
-#opt_design
-#place_design
-#report_clock_utilization -file $outputDir/clock_util.rpt
-#
-##get timing violations and run optimizations if needed
-#if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0.1} {
-#    puts "Found setup timing violations => running physical optimization"
-#    phys_opt_design
-#}
-#write_checkpoint -force $outputDir/post_place.dcp
-#report_utilization -file $outputDir/post_place_util.rpt
-#report_timing_summary -file $outputDir/post_place_timing_summary.rpt
-#
-##Route design and generate bitstream
-#route_design -directive Explore
-#write_checkpoint -force $outputDir/post_route.dcp
-#
-#write_bit_and_flash_images tube_power
+write_checkpoint -force $outputDir/post_synth.dcp
+report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
+report_utilization -file $outputDir/post_synth_util.rpt
+
+opt_design
+place_design
+report_clock_utilization -file $outputDir/clock_util.rpt
+
+#get timing violations and run optimizations if needed
+if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0.1} {
+    puts "Found setup timing violations => running physical optimization"
+    phys_opt_design
+}
+write_checkpoint -force $outputDir/post_place.dcp
+report_utilization -file $outputDir/post_place_util.rpt
+report_timing_summary -file $outputDir/post_place_timing_summary.rpt
+
+#Route design and generate bitstream
+route_design -directive Explore
+write_checkpoint -force $outputDir/post_route.dcp
+
+write_bit_and_flash_images tube_power
 
 save_project_as tube_power tube_power -force
